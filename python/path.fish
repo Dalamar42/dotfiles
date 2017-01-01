8c03c99d9d750767c4f7f9f9406a82772202e617
Thomas Maroulis <thomas@metabroadcast.com>
2017-01-01 23:05:35 +0000

    Fix deprecation warning from pip3 list

--- python/path.fish
+++ python/path.fish
@@ -3,6 +3,6 @@ if functions -q vf
     vf deactivate > /dev/null
 end
 
-if test (command -v python3) -a (pip3 list | grep virtualfish)
+if test (command -v python3) -a (pip3 list --format=legacy | grep virtualfish)
     eval (python3 -m virtualfish auto_activation global_requirements projects)
 end
