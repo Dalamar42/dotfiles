379744ff2b431006b99a110586cec0f2ca527ae7
Thomas Maroulis <thomas.maroulis@smarkets.com>
2017-05-31 18:57:28 +0100

    Add i3 configuration
    - Also fix tmux color scheme

---
+++ i3.configlink/screenlayout.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+xrandr --dpi 120 \
+    --output HDMI-3 --off \
+    --output HDMI-2 --off \
+    --output HDMI-1 --off \
+    --output DP-3 --off \
+    --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate right \
+    --output DP-1 --primary --mode 2560x1440 --pos 0x592 --rotate normal
+
