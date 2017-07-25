76e6d037b13377e8cd4a4c69601002c72cb1dbc0
Thomas Maroulis <thomas.maroulis@smarkets.com>
2017-07-25 15:01:46 +0100

    Update fish vim keybindings

--- fish.configlink/aliases.fish/fish_user_key_bindings.fish
+++ fish.configlink/aliases.fish/fish_user_key_bindings.fish
@@ -9,7 +9,15 @@ function fish_user_key_bindings
     bind -M visual j backward-char
     bind -M visual l forward-char
 
-    bind -m insert ' ' force-repaint
+    # useful insert mode mappings
+    bind -M insert \ch backward-delete-char
+    bind -M insert \cw backward-kill-word
+    bind -M insert \cu backward-kill-line
+    bind -M insert \cp history-search-backward
+    bind -M insert \cn history-search-forward
+    bind -M insert \cb backward-word
+    bind -M insert \cf forward-word   bind -m insert ' ' force-repaint
+    bind -M insert \cx end-of-line
 end
 
 funcsave fish_user_key_bindings
