1a0a023b66f53722ad40ca12843abe6c8cb0a89e
Thomas Maroulis <thomasmaroulis@gmail.com>
2019-02-10 14:57:55 +0000

    Fix broken python path.fish

--- python/path.fish
+++ python/path.fish
@@ -3,6 +3,8 @@ if functions -q vf
     vf deactivate > /dev/null
 end
 
-if test (command -v python3) -a (pip3 list --format=legacy | grep virtualfish)
-    eval (python3 -m virtualfish auto_activation global_requirements projects)
+if test (command -v python3)
+    if test (pip3 list --format=freeze | grep virtualfish)
+        eval (python3 -m virtualfish auto_activation global_requirements projects)
+    end
 end
