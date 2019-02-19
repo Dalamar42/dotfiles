0cf8287564de951356e9b059677ac2a8c78f4eb7
Thomas Maroulis <thomas.maroulis@smarkets.com>
2019-02-19 19:17:47 +0000

    Make nix packages support different systems

--- nix/packages.nix
+++ nix/packages.nix
@@ -1,34 +1,43 @@
 with import ./nixpkgs.nix {};
-[
-  # Nix
-  nix
-  cacert
+let
+  commonPkgs = [
+    # Nix
+    nix
+    cacert
+   
+    # Dev
+    tldr
+    rustup
+    vim
+    git
+   
+    # Shell
+    fish
+    fzf
+   
+    # Web
+    curl
+    httpie
+    jq
+    wget
+   
+    # System
+    htop
+   
+    # Utils
+    zip
+    unzip
+   
+    # Dotfiles
+    stow
+  ];
+  linuxPkgs = [
+    # Dev
+    kcachegrind
+  ];
+  darwinPkgs = [];
+in
+  commonPkgs ++
+  (lib.optionals stdenv.isLinux linuxPkgs) ++
+  (lib.optionals stdenv.isDarwin darwinPkgs)
 
-  # Dev
-  python37
-  python27
-  tldr
-  rustup
-  vim
-  git
-
-  # Shell
-  fish
-  fzf
-
-  # Web
-  curl
-  httpie
-  jq
-  wget
-
-  # System
-  htop
-
-  # Utils
-  zip
-  unzip
-
-  # Dotfiles
-  stow
-]
