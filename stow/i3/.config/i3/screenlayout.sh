633b90c36243a473d0500ccd620220cdccf6354e
Thomas Maroulis <thomas.maroulis@smarkets.com>
2019-08-10 18:52:47 +0100

    Add 3rd monitor

--- stow/i3/.config/i3/screenlayout.sh
+++ stow/i3/.config/i3/screenlayout.sh
@@ -2,7 +2,7 @@
 xrandr --dpi 120 \
     --output HDMI-3 --off \
     --output HDMI-2 --off \
-    --output HDMI-1 --off \
+    --output HDMI-1 --mode 2560x1440 --pos -1440x175 --rotate left \
     --output DP-3 --off \
     --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate right \
     --output DP-1 --primary --mode 2560x1440 --pos 0x592 --rotate normal
