e3206a9066ad3895b44f38e1bf1a5070d7650a5b
Thomas Maroulis <thomas.maroulis@smarkets.com>
2019-11-19 11:57:37 +0000

    Set gsettings options on i3 startup

---
+++ stow/i3/.config/i3/gsettings.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
+gsettings set org.gnome.desktop.session idle-delay 0
