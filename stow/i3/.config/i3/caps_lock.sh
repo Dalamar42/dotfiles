122b4e3b968058c28888b8939de1f5d8264f7321
Thomas Maroulis <thomasmaroulis@gmail.com>
2020-05-23 13:04:08 +0100

    Fix caps lock script

--- stow/i3/.config/i3/caps_lock.sh
+++ stow/i3/.config/i3/caps_lock.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-xmodmap -e 'clear lock' -e 'remove Lock = Caps_Lock' -e 'keysym Caps_Lock = Escape' -e 'keycode 0x42 = Escape'
+xmodmap -e 'clear lock' -e 'keycode 0x42 = Escape'
 
