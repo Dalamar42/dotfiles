a763d2b78dde48d9a194a8f9d449c46228e52c7d
Thomas Maroulis <thomasmaroulis@gmail.com>
2020-08-08 13:49:42 +0100

    Update screenlayout

--- stow/i3/.config/i3/screenlayout.sh
+++ stow/i3/.config/i3/screenlayout.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 xrandr --dpi 120 \
-    --output HDMI-3 --off \
-    --output HDMI-2 --off \
-    --output HDMI-1 --mode 2560x1440 --pos -1440x175 --rotate left \
-    --output DP-3 --off \
-    --output DP-2 --mode 2560x1440 --pos 2560x0 --rotate right \
-    --output DP-1 --primary --mode 2560x1440 --pos 0x592 --rotate normal
+    --output HDMI-A-0 --mode 1920x1080 --pos -1080x175 --rotate left \
+    --output DisplayPort-0 --primary --mode 2560x1440 --pos 0x380 --rotate normal
 
